cfa4a8d4b651ffff3eea6316c5348be5dd01b72a
Noel Smith <noelsmith@Noels-MacBook-Pro-2.local>
2025-04-10 09:16:57 -0400

    Level Four Complete

---
+++ python_review.py
@@ -0,0 +1,9 @@
+example = "I'm a string" # The variable called example is now holding the string value "I'm a string"
+a = 3
+b = 4.0
+c = Trued = False
+e = "Hey!"
+f = None
+age = 26
+name = "Nova"
+instrument = "Synths"
