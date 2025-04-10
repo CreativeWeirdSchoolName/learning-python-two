61d6d84e67407348a3be5ef072e771c894d9b403
Noel Smith <noelsmith@Noels-MacBook-Pro-2.local>
2025-04-10 09:54:20 -0400

    All Levels Complete, I think

--- python_review.py
+++ python_review.py
@@ -1,9 +1,46 @@
-example = "I'm a string" # The variable called example is now holding the string value "I'm a string"
-a = 3
-b = 4.0
-c = Trued = False
-e = "Hey!"
-f = None
-age = 26
-name = "Nova"
-instrument = "Synths"
+#example = "I'm a string" # The variable called example is now holding the string value "I'm a string"
+#a = 3
+#b = 4.0
+#c = Trued = False
+#e = "Hey!"
+#f = None
+#age = 26
+#name = "Nova"
+#instrument = "Synths"
+
+'''
+This is the first line of the comment
+This is the second
+'''
+
+'''
+prompt = "Enter your name: "
+
+response = input(prompt)
+
+print("Hello", input(response))
+
+print(response, " here is your agenda for the day...")
+'''
+
+#name = input("What is your name? ")
+#age = input("What is your age? ")
+
+#print(name, age, type(name), type,(age))
+
+#first_number = input("What's the first number? ")
+#second_number = input ("What's the seond number? ")
+
+#first_number = int(first_number)
+#second_number = int(second_number)
+
+#if first_number > second_number:
+#    print(first_number, "is bigger!!!!.")
+#elif first_number == second_number:
+#    print("The numbers are equal!!!!")
+#else:
+#    print(second_number, "is bigger!!!!")
+
+#name = input("What's your full name? ")
+#length_of_name = len(name)
+#print(length_of_name)
